ad679e5b42c8a721d2b86e92677c48dce741516c
Roshan <123867126+Roshu2003@users.noreply.github.com>
2026-09-08 22:08:54 +0530

    3870-count-commas-in-range Easy Math

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-       int ans = 0;
-       for(int i = 1000; i<= n; i++){
-        string s = to_string(i);
-        int cnt  = (s.size()) / 3;
-        int len = s.size();
-        if(len % 3 == 0)cnt--;
-        ans += cnt;
-       } 
-       return ans;
+        if( n < 1000)return 0;
+
+        return (n - 1000 + 1);
     }
 };
 // 2222222
