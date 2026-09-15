930c08ad355036ee6617b4fc1ef9a605002428a4
Roshan <123867126+Roshu2003@users.noreply.github.com>
2026-09-15 21:50:22 +0530

    2472-maximum-number-of-non-overlapping-palindrome-substrings Hard Two Pointers

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -3,21 +3,22 @@ public:
     string s;
     int k;
     int n;
-    bool isPalindrom(int i, int j){
-        int l = i;
-        int r = j;
-        while(l <= r){
-            if(s[l] != s[r])return false;
-            l++;
-            r--;
-        }
-        return true;
-    }
+    vector<vector<int>> isPalindrom;
+    // bool isPalindrom(int i, int j){
+    //     int l = i;
+    //     int r = j;
+    //     while(l <= r){
+    //         if(s[l] != s[r])return false;
+    //         l++;
+    //         r--;
+    //     }
+    //     return true;
+    // }
     int dp[2001][2001];
     int solve(int i,int j){
-        if(i >= n || j>= n)return 0;
+        if(i >= n || j >= n)return 0;
         if(dp[i][j] != -1)return dp[i][j];
-        if(isPalindrom(i,j)){
+        if(isPalindrom[i][j]){
             int take = 1 + solve(j + 1,j + k);
             int grow = solve(i,j + 1);
             int slide = solve(i + 1, j + 1);
@@ -33,6 +34,23 @@ public:
         k = k_;
         n = s.size();
         if(k == 1)return n;
+        isPalindrom.assign(n + 1,vector<int>(n + 1,0));
+        //to get all the palindrom substrng
+        for(int l = 1; l <= n; l++){
+            for(int i = 0; i + l <= n; i++){
+                int j = i + l  - 1;
+                //one length;
+                if(i == j){
+                    isPalindrom[i][j] = 1;
+                }
+                else if(i + 1 == j){
+                    isPalindrom[i][j] = (s[i] == s[j]);
+                }
+                else{
+                    isPalindrom[i][j] = (isPalindrom[i + 1][j - 1] && s[i] == s[j]);
+                }
+            }
+        }
         memset(dp,-1,sizeof(dp));
         return solve(0,k - 1);
     }
